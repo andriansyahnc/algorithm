cb525196cd9d79cd4612f876285221f1322c4e94
Muhammad Andriansyah Nur Cahya <andriansyahnc@users.noreply.github.com>
2017-05-11 00:50:56 +0800

    fizzbuzzwoof initialization

---
+++ 1_introduction/fizzbuzzwoof.py
@@ -0,0 +1,33 @@
+import argparse
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-l', '--limit', type=int,
+                        help="Limit the fizzbuzz to Xth counter", required=True)
+    return parser.parse_args()
+
+
+def fizzbuzz():
+    args = get_args()
+    limit = args.limit
+    
+    print_number = True
+    data = ""
+    if limit % 3 == 0:
+        data = data + "Fizz"
+        print_number = False
+    if limit % 5 == 0:
+        data = data + "Buzz"
+        print_number = False
+    if limit % 7 == 0:
+        data = data + "Woof"
+        print_number = False
+    if print_number is True:
+        data = data + str(limit)
+    
+    print(data)
+        
+
+if __name__ == "__main__":
+    fizzbuzz()
